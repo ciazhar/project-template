05482ffef113d3fc5df156ccf7cb1221458f8882
Muhammad Hafidz <ciazhar.id@gmail.com>
2022-01-27 17:29:47 +0700

    delete extension

--- db/init/20200727114046_add_extension.sql
+++ db/init/20200727114046_add_extension.sql
@@ -1,7 +1,4 @@
 -- +migrate Up
 CREATE EXTENSION if not exists "uuid-ossp" with schema extension;
-CREATE EXTENSION if not exists pgcrypto WITH SCHEMA extension;
-CREATE EXTENSION if not exists pg_trgm WITH SCHEMA extension;
-CREATE EXTENSION if not exists postgis WITH SCHEMA extension;
 -- +migrate Down
 
