ae87dfef06fee210caa9b3044ce1580f89cf04c4
Muhammad Hafidz <ciazhar.id@gmail.com>
2022-01-27 17:37:01 +0700

    delete migration

--- db/migrations/20211118213102-add_operator.sql
+++
@@ -1,3 +0,0 @@
--- +migrate Up
-create operator <-> (procedure = extension.geography_distance_knn, leftarg = extension.geography, rightarg = extension.geography, commutator = <->);
--- +migrate Down
